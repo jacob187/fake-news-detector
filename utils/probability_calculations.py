db90088e4ee86e075c879f54500e051cd0f495fb
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-04-18 12:47:33 -0400

    Add probability_calculations.py for calculating confidence scores using softmax function

---
+++ utils/probability_calculations.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def confidence_score(predictions):
+    return np.max(softmax(predictions), axis=1)
+
+
+def softmax(x: np.ndarray):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
