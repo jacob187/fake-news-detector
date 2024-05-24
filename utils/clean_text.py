9e099c008ca7a8eef5be9fae05465cb9fb9438c5
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-05-24 11:28:01 -0400

    Add type annotations to clean_text and probability_calculations functions

--- utils/clean_text.py
+++ utils/clean_text.py
@@ -2,7 +2,7 @@ import re
 
 
 # Function to clean text
-def clean_text(text):
+def clean_text(text: str) -> str:
     text = str(text)
     text = re.sub("\[.*?\]", "", text)
     text = re.sub("https?://\S+|www\.\S+", "", text)
--- utils/probability_calculations.py
+++ utils/probability_calculations.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-def confidence_score(predictions):
+def confidence_score(predictions: np.ndarray) -> np.ndarray:
     return np.max(softmax(predictions), axis=1)
 
 
-def softmax(x: np.ndarray):
+def softmax(x: np.ndarray) -> np.ndarray:
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0)
 
 
-def sigmoid(x):
+def sigmoid(x: np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(-x))
